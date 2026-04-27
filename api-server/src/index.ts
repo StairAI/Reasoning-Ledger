8f70b2012a1f424e2b083e1b8fe175bb30a7fcd4
Arslan Ablikim <arslan.ablikim0@gmail.com>
2026-04-27 15:42:40 +0800

    generate basic scaffolding for the sdk and api server

---
+++ api-server/src/index.ts
@@ -0,0 +1,40 @@
+import { createServer } from "node:http";
+import { OpenAPIHandler } from "@orpc/openapi/node";
+import { CORSPlugin } from "@orpc/server/plugins";
+import { onError } from "@orpc/server";
+import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
+import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
+import { router } from "./planet";
+
+const handler = new OpenAPIHandler(router, {
+  plugins: [
+    new CORSPlugin(),
+    new OpenAPIReferencePlugin({
+      schemaConverters: [new ZodToJsonSchemaConverter()],
+      specGenerateOptions: {
+        info: {
+          title: "ORPC Playground",
+          version: "1.0.0",
+        },
+      },
+    }),
+  ],
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+
+const server = createServer(async (req, res) => {
+  const result = await handler.handle(req, res, {
+    context: { headers: req.headers },
+  });
+
+  if (!result.matched) {
+    res.statusCode = 404;
+    res.end("No procedure matched");
+  }
+});
+
+server.listen(3000, "127.0.0.1", () => console.log("Listening on 127.0.0.1:3000"));
